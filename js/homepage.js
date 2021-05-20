4abd2fdfa3d0b70f7740b6be441aa2dfb0f8685d
Shreyas Pahune <pahuneshreyas@gmail.com>
2021-05-20 12:58:39 +0530

    added new svgs

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,16 @@
-let transaction = document.querySelector(".transactionA");
+const transaction = document.querySelector(".transactionA");
 
-transaction.addEventListener('click', ()=> {
-    document.querySelector('.toolTip').classList.toggle("toolTipShow");
-} )
+transaction.addEventListener("click", () => {
+  document.querySelector(".toolTip").classList.toggle("toolTipShow");
+});
+
+const lists = document.querySelectorAll(".list");
+
+for (let index = 0; index < lists.length; index++) {
+  lists[index].addEventListener("click", () => {
+    for (let i = 0; i < lists.length; i++) {
+      lists[i].classList.remove("active");
+    }
+    lists[index].classList.add("active");
+  });
+}
